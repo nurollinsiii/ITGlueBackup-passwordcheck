7b08906a5279d6d9156e890357b173e393d8e3aa
Neal R <94088135+nurollinsiii@users.noreply.github.com>
2022-04-25 09:31:46 -0500

    Create get-nurpasswordcomplexityresults.ps1
    This posh script will get the output of the ITGlueBackup Data and check the complexity of each password. Format and massage data from excel to meet your needs.

---
+++ get-nurpasswordcomplexityresults.ps1
@@ -0,0 +1,25 @@
+$report = [System.Collections.ArrayList]@()
+$files = Get-ChildItem *.xlsx -path "c:\Users\nealzo\Downloads\PasswordSample\*" -Recurse
+ForEach ($file in $files) {
+    $test = import-excel $file
+    $regex = "[^a-zA-Z0-9]"
+    $regex1 = "[a-z]"
+    $regex2 = "[A-Z]"
+    $regex3 = "[0-9]"
+    $test | % {
+        $objDat = [PSCustomObject]@{
+            organization      = $_."organization-name"
+            Name              = $_.name
+            username          = $_.username
+            url               = $_.url
+            password1         = $_.password.length
+            Password          = $_.password
+            SpecialCharacters = $_.password -cmatch $regex
+            LowerCase         = $_.password –cmatch $regex1
+            UpperCase         = $_.password –cmatch $regex2
+            Numbers           = $_.password –cmatch $regex3
+        }    
+        $report.add($objDat) | Out-Null
+    }
+}
+$report | Export-excel -path "c:\Users\nealzo\Downloads\PasswordSample\Final_exported_list_$(Get-Date -f yyyy_MM_dd).xlsx"
